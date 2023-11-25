113ad082a34f2d489250e39aeb7995d20fc55e33
jackychan616 <82193805+jackychan616@users.noreply.github.com>
2023-11-25 16:57:59 +0800

    meta update

--- src/pages/_document.page.js
+++ src/pages/_document.page.js
@@ -4,10 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-      <title>FreeGPT</title>
+      <title>FreeGPT - A strong free GPT chat services</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name='description' content='A strong , free Ai chat services'/>
+      <meta name='og:description' content='A strong , free Ai chat services , proived GPT 3.5 turbo and GPT 4'/>
       </Head>
       <body>
         <Main />
